974ef9273eb671c343ff52246df382fe1d03c2d8
Sam Phillips <samdphillips@gmail.com>
2020-05-29 15:12:53 -0700

    Use absolute paths to boot files for now.

--- src/c/racket_init.c
+++ src/c/racket_init.c
@@ -10,9 +10,10 @@ GDN_EXPORT void godot_gdnative_init(godot_gdnative_init_options *p_options) {
     racket_boot_arguments_t boot_args;
     memset(&boot_args, 0, sizeof(boot_args));
 
-    boot_args.boot1_path = "./petite.boot";
-    boot_args.boot2_path = "./scheme.boot";
-    boot_args.boot3_path = "./racket.boot";
+    /* in reality we need to be able to find these packaged */
+    boot_args.boot1_path = "/home/sam/projects/racket-dev/racket-godot/racket-7.7/lib/petite.boot";
+    boot_args.boot2_path = "/home/sam/projects/racket-dev/racket-godot/racket-7.7/lib/scheme.boot";
+    boot_args.boot3_path = "/home/sam/projects/racket-dev/racket-godot/racket-7.7/lib/racket.boot";
     boot_args.exec_file = "godot";
     racket_boot(&boot_args);
 

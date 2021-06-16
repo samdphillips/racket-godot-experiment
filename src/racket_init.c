53e572bea0ea6a89aaab2532b7806eba7f578384
Sam Phillips <samdphillips@gmail.com>
2021-06-16 11:21:05 -0700

    Update to racket-8.0

--- src/racket_init.c
+++ src/racket_init.c
@@ -13,9 +13,9 @@ GDN_EXPORT void rkt_gdnative_init(godot_gdnative_init_options *p_options) {
     memset(&boot_args, 0, sizeof(boot_args));
 
     /* in reality we need to be able to find these packaged */
-    boot_args.boot1_path = "/home/sam/projects/racket-dev/racket-godot/racket-7.7/lib/petite.boot";
-    boot_args.boot2_path = "/home/sam/projects/racket-dev/racket-godot/racket-7.7/lib/scheme.boot";
-    boot_args.boot3_path = "/home/sam/projects/racket-dev/racket-godot/racket-7.7/lib/racket.boot";
+    boot_args.boot1_path = "/home/sam/projects/racket-dev/racket-godot/racket-8.0.0.3/lib/petite.boot";
+    boot_args.boot2_path = "/home/sam/projects/racket-dev/racket-godot/racket-8.0.0.3/lib/scheme.boot";
+    boot_args.boot3_path = "/home/sam/projects/racket-dev/racket-godot/racket-8.0.0.3/lib/racket.boot";
     boot_args.exec_file = "godot";
     racket_boot(&boot_args);
     declare_modules();
